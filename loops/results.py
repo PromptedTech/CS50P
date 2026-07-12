601995f59e93fa3728a94c28ec68a6c4f3cd8b53
PromptedTech <nakull.rajawat@gmail.com>
2026-07-12 12:38:25 +0530

    ts file demostrates how to use the list and how  various methods of it function!

---
+++ loops/results.py
@@ -0,0 +1,8 @@
+results = ['Mario', 'Luigi', 'Princess', 'Yoshi', 'Koopa Troopa', 'Toad', 'Bowser', 'Donkey Kong Jr.']
+
+results.remove('Bowser')
+results.insert(0, 'Bowser')
+results.reverse()
+results.extend(['Nakul', 'Shiva'])
+
+print(results)
