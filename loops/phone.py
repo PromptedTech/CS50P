8faae81ffb5af5b45d37bcd48576dcd3125026bf
PromptedTech <nakull.rajawat@gmail.com>
2026-07-13 09:28:35 +0530

    in ts project we r learning how to do str slicing, nothin heave to look for

---
+++ loops/phone.py
@@ -0,0 +1,8 @@
+def main():
+    phone = '617-495-1000'
+    # here while doing da str slicing the first value will be included in the slicing, 
+    # but the last value tells till where u wanna go and where u need to stop,
+    # so da last value wont be included!
+    print(phone[4:7])
+
+main()
