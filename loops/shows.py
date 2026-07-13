11e0fa05daad6baf120c2902b6a267304ed75a10
PromptedTech <nakull.rajawat@gmail.com>
2026-07-13 09:10:51 +0530

    In ts project we r learning how to use str method

---
+++ loops/shows.py
@@ -0,0 +1,13 @@
+SHOWS = [
+    'Avatar: the way of water',
+    '   jimmy neutron',
+    '  the proud family   ',
+    'kim possible',
+    '  spongebob squarepants'
+]
+
+def main():
+    for show in SHOWS:
+        print(show.strip().title())
+
+main()
