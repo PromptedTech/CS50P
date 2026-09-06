171ad334c8f57b9fa28322c1a46cdbfd9545cb1c
PromptedTech <nakull.rajawat@gmail.com>
2026-09-06 14:12:56 +0530

    learning how to make test for a particular program

---
+++ Unit Test/test_hello.py
@@ -0,0 +1,11 @@
+from hello import hello
+
+def test_name():
+    assert hello("Nakul") == "hello, Nakul"
+
+def test_default():
+    assert hello() == "hello, world"
+
+def test_argument():
+    for name in ["Hermione", "Harry", "Ron"]:
+        assert hello(name) == f"hello, {name}"
