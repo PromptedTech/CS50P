f305f9e273845b602292e48a9aafc13cd230cef5
PromptedTech <nakull.rajawat@gmail.com>
2026-09-09 12:15:22 +0530

    in this program we created a function by which when user inputs smth then we remove the vowel letters and output the the users inputed word without vowel

---
+++ pset5/twttr.py
@@ -0,0 +1,17 @@
+def main():
+    txt = input("Input: ")
+    print(shorten(txt))
+
+
+def shorten(word):
+    output = ""
+
+    for char in word:
+        if char.lower() not in "aeiou":
+            output += char
+
+    return output
+
+
+if __name__ == "__main__":
+    main()
